0e5a099ef54af773b6b5dde30bc1f9b0b5574e68
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-08-02 14:04:26 +0300

    fsdfdsfdsfdsfdsf

--- dist/server.js
+++ dist/server.js
@@ -6,12 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 const topicRoutes_1 = __importDefault(require("./routes/topicRoutes"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-mongoose_1.default.connect(process.env.MONGODB_URI);
+app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'public')));
+app.get('/', (_req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '..', 'public', 'index.html'));
+});
 app.use('/api/user', userRoutes_1.default);
 app.use('/api', topicRoutes_1.default);
+mongoose_1.default.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Mongo connection error:', err));
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,26 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from 'express';
+import path from 'path';
 import mongoose from 'mongoose';
+
 import userRoutes from './routes/userRoutes';
 import topicRoutes from './routes/topicRoutes';
-import path from 'path';
+
 const app = express();
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(express.json());
-mongoose.connect(process.env.MONGODB_URI!);
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 app.use('/api/user', userRoutes);
 app.use('/api', topicRoutes);
+
+mongoose.connect(process.env.MONGODB_URI!)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Mongo connection error:', err));
+
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
